870161c78308e66e0a2b7aa1b4bbfa44c67c71ba
Andrea <andreaevelim450@gmail.com>
2026-08-06 14:52:31 -0600

    Instalacion de Tailwind y configuracion inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud de Tareas</title>
+</head>
+<body>
+    <h1 class="uppercase text-2xl font-bold bg-blue-500 text-white p-4">Crud de Tareas</h1>
+</body>
+</html>
